4fb6dd0e634f93832c0ec429e292fce77ccfe8f3
MindaugsLibietis <48270606+MindaugsLibietis@users.noreply.github.com>
2019-03-06 06:54:26 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+print("Hello, this is my first calcolator")
+num1=int(input("enter your first number - "))
+func=input("function - + , - , * , / ")
+num2=int(input("enter your second number - "))
+
+if func== "+":
+    ans=num1+num2
+    print(ans)
+else:
+    pass
+
+if func== "-":
+    ans=num1-num2
+    print(ans)
+else:
+    pass
+
+if func== "*":
+    ans=num1*num2
+    print(ans)
+else:
+    pass
+
+if func== "/":
+    ans=num1/num2
+    print(ans)
+else:
+    pass
